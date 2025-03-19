f71b7f4c2865b1189a0a68f0ceb6591fe884e057
fda77 <fda77@users.noreply.github.com>
2025-03-19 18:19:31 +0100

    fix lcd4linux, disable G15

--- make/pkgs/lcd4linux/lcd4linux.mk
+++ make/pkgs/lcd4linux/lcd4linux.mk
@@ -18,7 +18,7 @@ $(PKG)_CONFIGURE_PRE_CMDS += $(call PKG_PREVENT_RPATH_HARDCODING,./configure)
 
 $(PKG)_CONFIGURE_OPTIONS += --disable-rpath
 $(PKG)_CONFIGURE_OPTIONS += --with-x=no
-$(PKG)_CONFIGURE_OPTIONS += --with-drivers=all,!LCDLinux,!LUIse,!RouterBoard,!serdisplib,!st2205,!VNC,!X11,!HD44780,!LPH7508,!M50530,!T6963,!Noritake,!T6963,!Sample
+$(PKG)_CONFIGURE_OPTIONS += --with-drivers=all,!LCDLinux,!LUIse,!RouterBoard,!serdisplib,!st2205,!VNC,!X11,!HD44780,!LPH7508,!M50530,!T6963,!Noritake,!T6963,!Sample,!G15
 $(PKG)_CONFIGURE_OPTIONS += --with-plugins=all,!dbus,!gps,!mpd,!mpris_dbus,!mysql,!netinfo,!qnaplog,!raspi,!wireless
 
 #$(PKG)_CONFIGURE_POST_CMDS += $(SED) -i "s,.*AM_V_CCLD.*.\(.\).lcd4linux_LDADD.*,& \1(EXTRA_LIBS)," Makefile;
