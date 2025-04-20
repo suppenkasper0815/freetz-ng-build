c84626c9554af6ceef64e205519ed97e999822a2
fda77 <fda77@users.noreply.github.com>
2025-04-20 15:37:17 +0200

    ca-bundle: put pem into pkg _DIR

--- make/pkgs/ca-bundle/ca-bundle.mk
+++ make/pkgs/ca-bundle/ca-bundle.mk
@@ -5,16 +5,22 @@ $(PKG)_SITE:=https://www.curl.se/ca,https://curl.haxx.se/ca
 ### WEBSITE:=https://www.curl.se/ca
 ### SUPPORT:=fda77
 
-$(PKG)_BINARY:=$(DL_DIR)/$($(PKG)_SOURCE)
+$(PKG)_BINARY:=$($(PKG)_DIR)/cacert.pem
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)/etc/ssl/certs/ca-bundle.crt
 
 $(PKG)_STARTLEVEL=30
 
 
+define $(PKG)_CUSTOM_UNPACK
+	cp -fa $(DL_DIR)/$(CA_BUNDLE_SOURCE) $(CA_BUNDLE_BINARY)
+endef
+
 ifneq ($(strip $(DL_DIR)/$(CA_BUNDLE_SOURCE)), $(strip $(DL_DIR)/$(CA_BUNDLE_HOST_SOURCE)))
 $(PKG_SOURCE_DOWNLOAD)
 endif
 $(PKG_UNPACKED)
+
+$($(PKG)_BINARY): $($(PKG)_DIR)/.unpacked
 
 $($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
 	$(INSTALL_FILE)
