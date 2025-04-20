eff5a5387f83a5052866fb11c00159cfbc76d4ad
fda77 <fda77@users.noreply.github.com>
2025-04-20 15:25:53 +0200

    ca-bundle-host: put pem into pkg _DIR

--- make/host-tools/ca-bundle-host/ca-bundle-host.mk
+++ make/host-tools/ca-bundle-host/ca-bundle-host.mk
@@ -5,14 +5,18 @@ $(PKG)_SITE:=https://www.curl.se/ca,https://curl.haxx.se/ca
 ### WEBSITE:=https://www.curl.se/ca
 ### SUPPORT:=fda77
 
-$(PKG)_BINARY:=$(DL_DIR)/$($(PKG)_SOURCE)
+$(PKG)_BINARY:=$($(PKG)_DIR)/cacert.pem
 $(PKG)_TARGET_BINARY:=$(TOOLS_DIR)/cacert.pem
 
 
-ifneq ($($(PKG)_SOURCE),$(WGET_HOST_SOURCE))
+define $(PKG)_CUSTOM_UNPACK
+        cp -fa $(DL_DIR)/$(CA_BUNDLE_HOST_SOURCE) $(CA_BUNDLE_HOST_BINARY)
+endef
+
 $(TOOLS_SOURCE_DOWNLOAD)
-endif
 $(TOOLS_UNPACKED)
+
+$($(PKG)_BINARY): $($(PKG)_DIR)/.unpacked
 
 $($(PKG)_TARGET_BINARY): $($(PKG)_BINARY)
 	$(INSTALL_FILE)
@@ -23,6 +27,7 @@ $(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
 $(pkg)-clean:
 
 $(pkg)-dirclean:
+	$(RM) -r $(CA_BUNDLE_HOST_DIR)
 
 $(pkg)-distclean: $(pkg)-dirclean
 	$(RM) $(CA_BUNDLE_HOST_TARGET_BINARY)
