3d2774cd8ce64eec93f8729e5b82218060a8b109
fda77 <fda77@users.noreply.github.com>
2025-04-24 15:33:41 +0200

    bump unbound

--- docs/CHANGELOG.md
+++ docs/CHANGELOG.md
@@ -107,6 +107,7 @@ Latest changes
    * tcpdump 4.1.1/4.99.4
    * Tor 0.4.8.16
    * uMurmur 0.2.20/0.3.1
+   * Unbound 1.23.0
    * UnRAR 6.1.7/7.1.6
    * xsltproc 1.1.43
 
--- make/pkgs/unbound/unbound.mk
+++ make/pkgs/unbound/unbound.mk
@@ -1,7 +1,7 @@
-$(call PKG_INIT_BIN, 1.22.0)
-$(PKG)_LIB_VERSION:=8.1.30
+$(call PKG_INIT_BIN, 1.23.0)
+$(PKG)_LIB_VERSION:=8.1.31
 $(PKG)_SOURCE:=$(pkg)-$($(PKG)_VERSION).tar.gz
-$(PKG)_HASH:=c5dd1bdef5d5685b2cedb749158dd152c52d44f65529a34ac15cd88d4b1b3d43
+$(PKG)_HASH:=959bd5f3875316d7b3f67ee237a56de5565f5b35fc9b5fc3cea6cfe735a03bb8
 $(PKG)_SITE:=https://www.unbound.net/downloads
 ### WEBSITE:=https://www.unbound.net
 ### MANPAGE:=https://www.unbound.net/documentation/unbound.html
