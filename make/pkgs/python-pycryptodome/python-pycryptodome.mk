74cf3cc1d24455453bd39972a15b01a0ccd9580c
fda77 <fda77@users.noreply.github.com>
2025-05-19 17:11:35 +0200

    pycryptodome: update docs

--- make/pkgs/python-pycryptodome/python-pycryptodome.mk
+++ make/pkgs/python-pycryptodome/python-pycryptodome.mk
@@ -2,12 +2,17 @@ $(call PKG_INIT_BIN, 3.22.0)
 $(PKG)_SOURCE:=pycryptodome-$($(PKG)_VERSION).tar.gz
 $(PKG)_SITE:=https://files.pythonhosted.org/packages/source/p/pycryptodome
 $(PKG)_HASH:=fd7ab568b3ad7b77c908d7c3f7e167ec5a8f035c64ff74f10d47a4edd043d723
+### WEBSITE:=https://www.pycryptodome.org/
+### MANPAGE:=https://www.pycryptodome.org/src/api
+### CHANGES:=https://www.pycryptodome.org/src/changelog
+### CVSREPO:=https://github.com/Legrandin/pycryptodome/
 
 $(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)$(PYTHON_SITE_PKG_DIR)/Crypto/Hash/_SHA256.so
 
 $(PKG)_DEPENDS_ON += python gmp
 
 $(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_PYTHON_STATIC
+
 
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
@@ -20,6 +25,7 @@ $($(PKG)_TARGET_BINARY): $($(PKG)_DIR)/.configured
 $(pkg):
 
 $(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+
 
 $(pkg)-clean:
 	$(RM) -r $(PYTHON_PYCRYPTODOME_DIR)/build
