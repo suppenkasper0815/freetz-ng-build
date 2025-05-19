24f587228e67664d0da25714a18694277716d2c8
fda77 <fda77@users.noreply.github.com>
2025-05-19 21:12:55 +0200

    python-cffi: use flag file

--- make/pkgs/python-cffi/python-cffi.mk
+++ make/pkgs/python-cffi/python-cffi.mk
@@ -7,26 +7,27 @@ $(PKG)_HASH:=d400bfb9a37b1351253cb402671cea7e89bdecc294e8016a707f6d1d8ac934f9
 ### CHANGES:=https://github.com/python-cffi/cffi/releases
 ### CVSREPO:=https://github.com/python-cffi/cffi
 
-$(PKG)_TARGET_BINARY:=$($(PKG)_DEST_DIR)$(PYTHON_SITE_PKG_DIR)/_cffi_backend.so
-
 $(PKG)_DEPENDS_ON += python
 $(PKG)_DEPENDS_ON += python2-host python2-setuptools-host
 
 $(PKG)_REBUILD_SUBOPTS += FREETZ_PACKAGE_PYTHON_STATIC
 
+
 $(PKG_SOURCE_DOWNLOAD)
 $(PKG_UNPACKED)
 $(PKG_CONFIGURED_NOP)
 
-$($(PKG)_TARGET_BINARY): $($(PKG)_DIR)/.configured
+$($(PKG)_DIR)/.compiled: $($(PKG)_DIR)/.configured
 	$(call Build/PyMod/PKG, PYTHON_CFFI, , CFLAGS="$(TARGET_CFLAGS) -I$(TARGET_TOOLCHAIN_STAGING_DIR)/include/python2.7" CPPFLAGS="$(TARGET_CFLAGS) -I$(TARGET_TOOLCHAIN_STAGING_DIR)/include/python2.7" PYTHON_INCDIR="$(TARGET_TOOLCHAIN_STAGING_DIR)/include/python2.7" TARGET_ARCH_BE="$(TARGET_ARCH_BE)" PYTHONHOME=$(HOST_TOOLS_DIR)/usr)
-	touch -c $@
+	@touch $@
 
 $(pkg):
 
-$(pkg)-precompiled: $($(PKG)_TARGET_BINARY)
+$(pkg)-precompiled: $($(PKG)_DIR)/.compiled
+
 
 $(pkg)-clean:
+	$(RM) $(PYTHON_CFFI_DIR)/{.configured,.compiled}
 	$(RM) -r $(PYTHON_CFFI_DIR)/build
 
 $(pkg)-uninstall:
