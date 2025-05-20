7af58909742e9c74d8c077c88f3d1dcc8e046a0d
fda77 <fda77@users.noreply.github.com>
2025-05-20 12:10:44 +0200

    python3-packaging-host: update _TARGET_DIRECTORY

--- make/host-tools/python3-packaging-host/python3-packaging-host.mk
+++ make/host-tools/python3-packaging-host/python3-packaging-host.mk
@@ -12,14 +12,14 @@ $(PKG)_SITE:=https://github.com/pypa/packaging/archive/refs/tags
 $(PKG)_DEPENDS_ON+=python3-host
 
 $(PKG)_DIRECTORY:=$($(PKG)_DIR)/src/packaging
-$(PKG)_TARGET_DIRECTORY:=$(HOST_TOOLS_DIR)/usr/lib/python$(call GET_MAJOR_VERSION,$(PYTHON3_HOST_VERSION))/site-packages/packaging
+$(PKG)_TARGET_DIRECTORY:=$(HOST_TOOLS_DIR)/usr/lib/python$(call GET_MAJOR_VERSION,$(PYTHON3_HOST_VERSION))/site-packages
 
 
 $(TOOLS_SOURCE_DOWNLOAD)
 $(TOOLS_UNPACKED)
 
 $($(PKG)_DIR)/.installed: $($(PKG)_DIR)/.unpacked
-	cp -fa $(PYTHON3_PACKAGING_HOST_DIRECTORY) $(dir $(PYTHON3_PACKAGING_HOST_TARGET_DIRECTORY))
+	cp -fa $(PYTHON3_PACKAGING_HOST_DIRECTORY) $(PYTHON3_PACKAGING_HOST_TARGET_DIRECTORY)
 	@touch $@
 
 $(pkg)-precompiled: $($(PKG)_DIR)/.installed
@@ -31,6 +31,6 @@ $(pkg)-dirclean:
 	$(RM) -r $(PYTHON3_PACKAGING_HOST_DIR)
 
 $(pkg)-distclean: $(pkg)-dirclean
-	$(RM) -r $(PYTHON3_PACKAGING_HOST_TARGET_DIRECTORY)
+	$(RM) -r $(PYTHON3_PACKAGING_HOST_TARGET_DIRECTORY)/packaging
 
 $(TOOLS_FINISH)
