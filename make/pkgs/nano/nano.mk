5e2d5f204d6db947d1219e2fcd32813d51519be6
fda77 <fda77@users.noreply.github.com>
2025-08-25 15:32:35 +0200

    fix nano maybe

--- make/pkgs/nano/nano.mk
+++ make/pkgs/nano/nano.mk
@@ -49,9 +49,11 @@ $(PKG)_CONFIGURE_OPTIONS += $(if $(FREETZ_PACKAGE_NANO_MULTIBUFFER),--enable-mul
 $(PKG)_CONFIGURE_OPTIONS += $(if $(FREETZ_PACKAGE_NANO_COLOR_SYNTAX),--enable-color,--disable-color)
 $(PKG)_CONFIGURE_OPTIONS += $(if $(FREETZ_PACKAGE_NANO_NANORC),--enable-nanorc,--disable-nanorc)
 
+$(PKG)_CONFIGURE_ENV += ac_cv_header_libintl_h=no
 $(PKG)_CONFIGURE_ENV += ac_cv_func_strcasecmp=no
 $(PKG)_CONFIGURE_ENV += ac_cv_func_strncasecmp=no
-$(PKG)_CONFIGURE_ENV += ac_cv_header_libintl_h=no
+$(PKG)_CONFIGURE_ENV += ac_cv_func_getrandom=no
+$(PKG)_CONFIGURE_ENV += gl_cv_onwards_func_getrandom=yes
 
 $(PKG)_SYNTAX_FILES_LIST:=
 $(PKG)_SYNTAX_FILES_LIST += $(if $(FREETZ_PACKAGE_NANO_SYNTAX_FILE_SH),sh.nanorc)
